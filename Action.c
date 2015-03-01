da55d996c28c4fce99d269355adf91bd1512090a
Glubzite <glubzite@localhost.localdomain>
2015-03-01 17:07:18 +0100

    -(switch) +(if forest)

--- Action.c
+++ Action.c
@@ -5,7 +5,7 @@
 ** Login   <denel-_l@epitech.net>
 **
 ** Started on  Tue Feb 24 16:12:31 2015 denel-_l
-** Last update Fri Feb 27 15:46:16 2015 denel-_l
+** Last update Sun Mar  1 17:04:03 2015 Glubzite
 */
 
 #include "Action.h"
@@ -101,17 +101,25 @@ void		*doSomeThing(void *arg)
       tmp = (t_philo*)arg;
       if (tmp->priority == true)
 	getPriority(arg);
-      switch (tmp->act)
-	{
-	case NOTHING: doEat(arg);
-	  break;
-	case EAT: doSleep(arg);
-	  break;
-	case SLEEP: doEat(arg);
-	  break;
-	case THINK: doEat(arg);
-	  break;
-	}
+      /* if (tmp->act) */
+      /* 	{ */
+	  if (tmp->act == NOTHING)
+	    doEat(arg);
+	  /* case NOTHING: doEat(arg); */
+	  /*   break; */
+	  else if (tmp->act == EAT)
+	    doSleep(arg);
+	  /* case EAT: doSleep(arg); */
+	  /*   break; */
+	  else if (tmp->act == SLEEP)
+	    doEat(arg);
+	  /* case SLEEP: doEat(arg); */
+	  /*   break; */
+	  else if (tmp->act == THINK)
+	    doEat(arg);
+	  /* case THINK: doEat(arg); */
+	  /*   break; */
+	/* } */
     }
   return (arg);
 }
